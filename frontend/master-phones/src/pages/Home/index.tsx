655c44c9a7244ac4e1d635b6436f587c142bbd51
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-29 09:17:01 -0300

    start to create form-login page

--- frontend/master-phones/src/pages/Home/index.tsx
+++ frontend/master-phones/src/pages/Home/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
+    <div className="container first-box-form">
       <br/>
-      <Link to="/produtos"><Button variant="dark">Ver Produtos</Button></Link>
+      <Form>
+        <h4 className="subtitle-form">Faça Login para gerenciar os protudos</h4>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Digite seu Email"/>
+        </Form.Group>
+
+        <Form.Group controlId="Senha">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Digite sua Senha" />
+        </Form.Group>
+      </Form>
       <Link to="/gerenciar"><Button variant="dark">Gerenciar Produtos</Button></Link>
       <br/>
     </div>
+    <hr style={{width: "45%"}}/>
+    <div className="container second-box-form">
+      <h4 className="subtitle-form">Visualizar os Produtos</h4>
+      <Link to="/produtos"><Button variant="dark">Ver Produtos</Button></Link>
+    </div>
+    </>
   )
 }
